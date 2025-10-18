12e6b92d8c292df8f6e95eb2ff2f9495e7bb0a0f
BabySafrrr <habib.safir2020@gmail.com>
2025-10-18 16:25:16 -0600

    second

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { navLinks } from '../constants';
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <img src="/logo.svg" alt="Apple logo" />
+                <ul>
+                    {navLinks.map((link)=>(<li key={link.label}>
+                        <a href={link.label}>{link.label}</a>
+                    </li>))}
+                </ul>
+                <div className='flex-center gap-3'>
+                        <button>
+                            <img src="/search.svg" alt="Search" />
+                        </button>
+                        <button>
+                            <img src="/cart.svg" alt="cart" />
+                        </button>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
