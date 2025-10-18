12c54e019bcf77ee3f5357dcc9489fe6efd02b39
BabySafrrr <habib.safir2020@gmail.com>
2025-10-18 16:58:22 -0600

    Implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 const App = () => {
     return (
         <main>
             <Navbar></Navbar>
+            <Hero></Hero>
         </main>
     );
 }
